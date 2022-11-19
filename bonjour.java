93ad6fba282d8cc7b7fb72402542f40955882f5b
RajaBarhoumi <95989009+RajaBarhoumi@users.noreply.github.com>
2022-11-19 18:24:23 +0100

    Delete bonjour.java

--- bonjour.java
+++
@@ -1,9 +0,0 @@
-
-public class bonjour {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println ("Bonjour ! Voici mon 1er programme Java") ;
-	}
-
-}
